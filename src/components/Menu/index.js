d8578c6587d59ab42a4ec24c3eed07816e726ddb
Douglas Carlini <douglascarlini@gmail.com>
2019-05-04 16:32:14 -0300

    Arquivos adicionados

---
+++ src/components/Menu/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import QRCode from 'react-native-qrcode';
+
+import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles';
+
+export default function Menu({ translateY }) {
+    return (
+        <Container style={{
+            opacity: translateY.interpolate({
+                inputRange: [0, 150],
+                outputRange: [0, 1],
+            })
+        }}>
+            <Code>
+                <QRCode value="http://douglascarlini.com" size={80} bgColor="#8B10AE" fgColor="#FFF" />
+            </Code>
+
+            <Nav>
+                <NavItem>
+                    <Icon name="help-outline" size={20} color="#FFF" />
+                    <NavText>Me Ajuda</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="person-outline" size={20} color="#FFF" />
+                    <NavText>Perfil</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="credit-card" size={20} color="#FFF" />
+                    <NavText>Configurar Cartão</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="smartphone" size={20} color="#FFF" />
+                    <NavText>Configurar App</NavText>
+                </NavItem>
+            </Nav>
+
+            <SignOutButton onPress={() => { }}>
+                <SignOutButtonText>Sair</SignOutButtonText>
+            </SignOutButton>
+        </Container>
+    );
+}
